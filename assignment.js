a801f7e78ff3ff85fae4923b4915f1678e896f94
TabithaAluel <alueltabitha@gmail.com>
2021-03-07 12:10:52 +0300

    my first commit

---
+++ assignment.js
@@ -0,0 +1,7 @@
+function fruits(){
+    var fruit_list=["mango","tomato","strawberry","kiwi","guava"];
+     fruit_list.forEach(element => console.log(element));
+    console.log(fruit_list[3]);
+    
+}
+fruits();
